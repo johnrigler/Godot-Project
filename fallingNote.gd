d4c1bc70a1dc5723154b9bdbc9d4701dd9f27562
CMartin <120353755+CMartin8@users.noreply.github.com>
2024-05-18 15:43:48 -0500

    Add files via upload

---
+++ fallingNote.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+signal play_me
+signal play_me_key_pressed
+
+@export var speed = 400
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
---
+++ main.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var note_scene: PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+	
+# note spawn when timer ends
+func _on_note_timer_timeout():
+	var note = note_scene.instantiate()
+	
+	var note_spawn_location = $NoteSpawner
+	
+	note.position = note_spawn_location.position
+	
+	var direction = PI / 2
+	
+	var velocity = Vector2(200.0, 0.0)
+	note.linear_velocity = velocity.rotated(direction)
+	
+	add_child(note)
